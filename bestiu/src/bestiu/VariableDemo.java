18914e17aa2e4709c268cb52a44da1582a2bf4c0
shaik suleman <shaiksuleman1102@gmail.com>
2024-09-10 00:42:57 +0530

    second commit

--- bestiu/src/bestiu/VariableDemo.java
+++ bestiu/src/bestiu/VariableDemo.java
@@ -2,21 +2,9 @@ package bestiu;
 
 public class VariableDemo {
 
-	int a=10;
-	void print()
-	{
-		String msg="hello";
-		System.out.println(msg);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
 	}
-
-              static String message="hello students" ;
-
-         public static void main(String[] args) {
-        	 VariableDemo obj=new VariableDemo(); 
-         System.out.println("the value of a is "+obj.a);
-          obj.print();
-          System.out.println(message);
-
-}
 
 }
