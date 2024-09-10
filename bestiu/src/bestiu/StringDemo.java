37071d8601cb98a8b7f2d2cb38041ee799f6ba6b
shaik suleman <shaiksuleman1102@gmail.com>
2024-09-10 18:52:40 +0530

    fourth  commit

--- bestiu/src/bestiu/StringDemo.java
+++ bestiu/src/bestiu/StringDemo.java
@@ -3,9 +3,7 @@ package bestiu;
 public class StringDemo {
 
 	public static void main(String[] args) {
-		
-			String str="hello i am shaik suleman";
-			System.out.println(str);
+		// TODO Auto-generated method stub
 
 	}
 
