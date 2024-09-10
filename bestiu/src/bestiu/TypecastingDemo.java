3cd9f9d0a43fd336b0704d09ee1bdf617463d356
shaik suleman <shaiksuleman1102@gmail.com>
2024-09-10 18:33:29 +0530

    third commit

---
+++ bestiu/src/bestiu/TypecastingDemo.java
@@ -0,0 +1,24 @@
+package bestiu;
+
+public class TypecastingDemo {
+
+	public static void main(String[] args) {
+		
+           byte b=10;
+           int i=b;
+           System.out.println(i);
+           
+           float f=22.14f;
+           double d=f;
+           System.out.println(d);
+           
+           double f1=10.52d;
+           long l= (long)f1;
+           System.out.println(1);
+           
+           float f2=34.56f;
+           int li=(int) f2;
+           System.out.println(li);
+	}
+
+}
