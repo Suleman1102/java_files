b9f4ec0ca635d12638423784f24b46fd3a162cf3
shaik suleman <shaiksuleman1102@gmail.com>
2024-09-16 19:00:10 +0530

    twelveth commit

---
+++ bestiu/src/bestiu/IncrementDecrementDemo.java
@@ -0,0 +1,18 @@
+package bestiu;
+
+public class IncrementDecrementDemo {
+
+	public static void main(String[] args) {
+		   int a=10;
+		    System.out.println("++a :" + a++);
+		    System.out.println("after post-increment of a" + a);
+		    
+		    System.out.println("++a :" + ++a);
+		    
+		    System.out.println("a--:" + a--);
+		    System.out.println("after post-decrement of a" + a);
+		    
+		    System.out.println("--a" + --a);
+	}
+
+}
